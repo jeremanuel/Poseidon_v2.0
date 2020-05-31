418a08ce848a3008b301d1fbe95ac8b017d941e9
AgusMassa <agus.massa@hotmail.com>
2020-05-31 18:50:21 -0300

    Test Main with Drag images

--- db/dbLogin.js
+++ db/dbLogin.js
@@ -5,8 +5,7 @@ let DB = new DTB.Database('./db/database.');
 DB.run('CREATE TABLE IF NOT EXISTS usuarios(email text PRIMARY KEY, pass text NOT NULL)');
 
 
-function insert(email , password)
-{
+function insert(email , password){
     DB.run('insert into usuarios(email,pass) values(?,?)',email, password, function(err)
     {
         if(err)
@@ -15,5 +14,16 @@ function insert(email , password)
 
     console.log('se insereto esa');
 }
-    
+
+function delet(email, password){
+    if ((authentication(email,password))) {
+        DB.run('delete from usuarios where email =' + email, function(err){
+            if(err)
+                console.log('error xd');
+        });
+    }
+    console.log('se elimino ');
+}
+
     module.exports.insert = insert;
+    module.exports.delet = delet;
